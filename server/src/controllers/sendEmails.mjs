c9a6107133823797d83c056ddf1cedf4032a9e08
Veer <bveee369@gmial.com>
2025-11-06 19:15:14 +0530

    m

--- server/src/controllers/sendEmails.mjs
+++ server/src/controllers/sendEmails.mjs
@@ -2,28 +2,31 @@ import { transporter } from "../config/nodeMailer.mjs";
 import { asyncHandler } from "../utils/asyncHandler.mjs";
 
 
- export const sendEmail =asyncHandler(async (req,res) => {
-    
-     const {to, subject,massage} = req.body
-     console.log(to,subject,massage)
-   try {
-     const mailOption ={
-         from:"bveee369@gmail.com",
-         to, 
-         subject,
-         text:massage
-     }
-       const  mail = await  transporter.sendMail(mailOption);
-         res.status(200).json({
-         massage:"Email send successfully",
-         success:true
-       })
-   } catch (error) {
-     
-     res.status(500).json({
-         massage:"Email sending failled",
-         success:false
-       })
-   }
+export const sendEmail = asyncHandler(async (req, res) => {
+
+  const { to, subject , massage } = req.body
+  
+  console.log(to, subject, massage)
+  try {
+    const mailOption = {
+      from: "bveee369@gmail.com",
+      to,
+      subject,
+      text: massage,
+    }
+
+    const mail = await transporter.sendMail(mailOption);
+    console.log("work")
+    res.status(200).json({
+      massage: "Email send successfully",
+      success: true
+    })
+  } catch (error) {
+
+    res.status(500).json({
+      massage: "Email sending failled",
+      success: false
+    })
+  }
 
 })
